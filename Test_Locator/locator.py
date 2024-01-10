2173b1286a34cd669c45b9a31b502b5507230aa5
Sheema-parveen <sheemaparveenfeb14@gmail.com>
2024-01-10 19:55:51 +0530

    All locators are stored here

---
+++ Test_Locator/locator.py
@@ -0,0 +1,19 @@
+class Locator():
+    Name = "name" 
+    Email= "email"
+    Number= "phone"
+    Address= "textarea"
+    Gender="female"
+    Day1="friday"
+    Day2="monday"
+    Day3 ="thursday"
+    deselectday="saturday"
+    Country="country"
+    India='//option[@value="india"]' #xpath
+    Colors="colors"
+    White='//option[@value="white"]' #xpath
+    Date="datepicker"
+    previous='//*[@id="ui-datepicker-div"]/div/a[1]/span'
+    december='//*[@id="ui-datepicker-div"]/table/tbody/tr[3]/td[7]/a'
+    opencart='//a[@href="https://demo.opencart.com/"]'
+    homelink='//*[contains(text(),"Home")]'
