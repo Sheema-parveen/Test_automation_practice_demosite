a091924719a2a41e86a931e9b3e087e441060a0e
Sheema-parveen <sheemaparveenfeb14@gmail.com>
2024-01-10 19:54:47 +0530

    All the values are stored here

---
+++ Test_Data/data.py
@@ -0,0 +1,6 @@
+class Data:
+    url="https://testautomationpractice.blogspot.com/"
+    Name_textbox = "sheema" 
+    Email_textbox= "sheema@gmail.com"
+    Number_textbox= "0987654321"
+    Address_textarea ="No:5, Thiruvanmaiyur, Chennai"
